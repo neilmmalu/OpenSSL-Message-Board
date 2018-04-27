40dd19d42e19989f6481c904aa12548b718d2e4e
Neil Malu <Neil@Neils-MacBook-Pro.local>
2018-04-27 13:04:17 -0400

    Added comments

--- server.py
+++ server.py
@@ -8,10 +8,13 @@ HOST = 'localhost'
 PORT = 8080
 
 def main():
+
+	# create and get the socket to listen to clients
 	s = socket.socket()
 	s.bind((HOST, PORT))
 	s.listen(10)
 
+	# wrap socket in the cert
 	ssl_s = ssl.wrap_socket(s, certfile = 'domain.crt', keyfile = 'domain.key', server_side = True)
 	while True:
 		client_socket, client_address = ssl_s.accept()
@@ -19,6 +22,7 @@ def main():
 
 		try:
 			print "Server running"
+			# spin a new thread for a new client
 			thread.start_new_thread(handle, (client_socket,))
 		except ssl.SSLError as e:
 			print(e)
@@ -33,6 +37,8 @@ def handle(conn):
     password = conn.recv(1024).decode()
 
     success = verify(username, password)
+
+    # verify if the username password combination is right
     if success == 1:
     	conn.send('SUCCESS')
     	# Do stuff with queries here
@@ -46,15 +52,20 @@ def handle(conn):
 def verify(username, password):
 	file = open('passwords.txt', 'r')
 	line = file.readline()
+	# line number
 	i = 1;
 	while line:
+		# even lines are passwords 
 		if i % 2 == 0:
 			line = file.readline()
 			i += 1
 		else:
 			user = line.strip('\n')
+			# check if the line has the user 
 			if user == username:
 				hashed = file.readline().strip('\n')
+
+				# check if passwords match
 				if(sha256(password).hexdigest() == hashed):
 					# print "Success. Welcome " + username + '\n'
 					return 1
@@ -66,6 +77,8 @@ def verify(username, password):
 				i += 1
 
 	file.close()
+
+	# create a new user if the username does not exist
 	file = open('passwords.txt', 'w')
 	new_hash = sha256(password).hexdigest()
 	file.write(username+'\n'+new_hash+'\n')
@@ -77,15 +90,18 @@ def queries(conn, username):
 	conn.send("ACK queries".encode())
 	info = conn.recv(1024).decode()
 
+	# info can be END, GET or POST
 	while info != 'END':
 		info = conn.recv(1024).decode()
 		if info == 'END':
+			# Logout user but keep the connection
 			print "Logging out\n"
 			conn.send('END'.encode())
 		
 		elif info == 'GET':
 			conn.send('GET'.encode())
 			group = conn.recv(1024).decode()
+			# get messages from the JSON file and add them to a buffer
 			messages = get_messages(group)
 			buff = ""
 			for i in range(len(messages)):
@@ -96,6 +112,8 @@ def queries(conn, username):
 			conn.send('POST'.encode())
 			group = conn.recv(1024).decode()
 			message = conn.recv(4096).decode()
+
+			# post the message to the JSON file 
 			post_message(username, group, message)
 
 		else:
